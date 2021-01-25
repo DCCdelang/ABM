d1c8ed4d769d6a4aa4ad71fe89cf3c9b7cbfa1cc
DCCdelang <33282294+DCCdelang@users.noreply.github.com>
2021-01-25 12:14:28 +0100

    changes

--- epstein_civil_violence_Normal+Network Grid/plot_SA.py
+++ epstein_civil_violence_Normal+Network Grid/plot_SA.py
@@ -13,13 +13,54 @@ problem = {
     'names': ['links', 'citizen_vision', 'cop_vision', 'max_jail_term'],
     'bounds': [[1, 7], [1, 10], [1, 10], [1, 50]]
 }
+# param_Cat = param_values[0:120]
+# param_Dante = param_values[180:200] # 140,160,180,200,220,240
+# param_Kamiel = param_values[240:360]
+# param_Louky = param_values[360:480]
+# param_Ignas = param_values[480:]
 
-data_from_csv = pd.read_csv("Old_SA/SA_data_NAME.csv")
+cat_1 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_1/SA_data_Cat_1.csv")
+cat_2 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_2/SA_data_Cat_2.csv")
+cat_3 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_3/SA_data_Cat_3.csv")
+cat_4 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_4/SA_data_Cat_4.csv")
+cat_5 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_5/SA_data_Cat_5.csv")
+cat_6 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Cat/SA_data_Cat_6/SA_data_Cat_6.csv")
+
+Cat_Total = pd.concat([cat_1,cat_2,cat_3,cat_4,cat_5,cat_6])
+print(Cat_Total.shape)
+
+dante_1 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_120_140.csv")
+dante_2 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_140_160.csv")
+dante_3 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_160_180.csv")
+dante_4 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_180_200.csv")
+dante_5 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_200_220.csv")
+dante_6 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Dante/SA_data_220_240.csv")
+
+Dante_Total = pd.concat([dante_1,dante_2,dante_3,dante_4,dante_5,dante_6])
+print(Dante_Total.shape)
+
+iggy_1 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data1.csv")
+iggy_2 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data2.csv")
+iggy_3 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data3.csv")
+iggy_4 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data4.csv")
+iggy_5 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data5.csv")
+iggy_6 = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Iggy/SA_data6.csv")
+
+Iggy_Total = pd.concat([iggy_1,iggy_2,iggy_3,iggy_4,iggy_5,iggy_6])
+print(Iggy_Total.shape)
+
+Kamiel_Total = pd.read_csv("epstein_civil_violence_Normal+Network Grid/SA_data_Kamiel/SA_data_Kamiel.csv")
+print(Kamiel_Total.shape)
+
+Total_SA = pd.concat([Cat_Total,Dante_Total,Iggy_Total,Kamiel_Total]).fillna(0)
+print(Total_SA.shape)
+print(Total_SA.head())
 #%%
-Si_active = sobol.analyze(problem, data_from_csv['perc_time_rebel'].values, print_to_console=True, calc_second_order=False)
-Si_legitimacy = sobol.analyze(problem, data_from_csv['mean_peak_size'].values, print_to_console=True, calc_second_order=False)
-Si_peaks = sobol.analyze(problem, data_from_csv['Peaks'].values, print_to_console=True, calc_second_order=False)
-Si_peaks = sobol.analyze(problem, data_from_csv['Peaks'].values, print_to_console=True, calc_second_order=False)
+Si_active = sobol.analyze(problem, Total_SA['perc_time_rebel'].values, print_to_console=True, calc_second_order=False)
+Si_calm = sobol.analyze(problem, Total_SA['perc_time_calm'].values, print_to_console=True, calc_second_order=False)
+Si_legitimacy = sobol.analyze(problem, Total_SA['mean_peak_size'].values, print_to_console=True, calc_second_order=False)
+Si_peak_interval = sobol.analyze(problem, Total_SA['mean_peak_interval'].values, print_to_console=True, calc_second_order=False)
+Si_peaks = sobol.analyze(problem, Total_SA['Peaks'].values, print_to_console=True, calc_second_order=False)
 
 def plot_index(s, params, i, title=''):
     """
@@ -54,14 +95,14 @@ def plot_index(s, params, i, title=''):
     plt.errorbar(indices, range(l), xerr=errors, linestyle='None', marker='o')
     plt.axvline(0, c='k')
 
-for Si in (Si_active, Si_legitimacy, Si_peaks):
+Si = [Si_active, Si_calm, Si_legitimacy, Si_peaks, Si_peak_interval]
+title = ["Si_active", "Si_calm", "Si_legitimacy", "Si_peaks", "Si_peak_interval"]
+
+for i in range(len(Si)):
     # First order
-    plot_index(Si, problem['names'], '1', 'First order sensitivity')
+    plot_index(Si[i], problem['names'], '1', 'First order '+title[i])
     plt.show()
 
     # Total order
-    plot_index(Si, problem['names'], 'T', 'Total order sensitivity')
+    plot_index(Si[i], problem['names'], 'T', 'Total order '+title[i])
     plt.show()
-
-
-# %%
