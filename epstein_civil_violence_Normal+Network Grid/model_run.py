7607f300d95e6404043b51ea9adfd739d90fee87
louky123 <loukyschutte@live.nl>
2021-01-28 08:20:09 +0100

    new data legitimacy Fixed and Global

--- epstein_civil_violence_Normal+Network Grid/model_run.py
+++ epstein_civil_violence_Normal+Network Grid/model_run.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-#%%
-=======
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
 import matplotlib.pyplot as plt
 import importlib
 
@@ -9,34 +5,22 @@ from epstein_civil_violence.model import EpsteinCivilViolence
 from epstein_civil_violence.agent import Citizen, Cop
 
 import time
-<<<<<<< HEAD
-legitimacy_kind = "Local" # choose between "Fixed","Global","Local"
-=======
 legitimacy_kind = "Global" # choose between "Fixed","Global","Local"
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
 smart_cops = False
 cop_density = .04
 
 start = time.time()
-model = EpsteinCivilViolence(height=40, 
-                           width=40,
-<<<<<<< HEAD
-                           links = 5,
-=======
+model = EpsteinCivilViolence(height=100, 
+                           width=100,
                            links = 10,
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
                            citizen_density=.7, 
-                           cop_density=cop_density, 
+                           cop_density=0.04, 
                            citizen_vision=7, 
                            cop_vision=7, 
                            legitimacy=.82, 
                            max_jail_term=30, 
-<<<<<<< HEAD
-                           max_iters=500, # cap the number of steps the model takes
-=======
-                           max_iters=100, # cap the number of steps the model takes
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
-                           smart_cops = smart_cops,
+                           max_iters=180, # cap the number of steps the model takes
+                           smart_cops = False,
                            legitimacy_kind = legitimacy_kind, # choose between "Fixed","Global","Local"
                            max_fighting_time=1
                            ) 
@@ -54,11 +38,7 @@ ax.set_xlabel('Step')
 ax.set_ylabel('Number of Citizens')
 _ = ax.legend(bbox_to_anchor=(1.35, 1.025))
 plt.tight_layout()
-<<<<<<< HEAD
 plt.savefig(r"C:\Users\Gebruiker\OneDrive\Computational_Science\Year1_Semester1_Block3\ABM\figures_normalgrid/plot_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
-=======
-plt.savefig("figures_normalgrid/plot_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
 plt.show()
 if legitimacy_kind != "Local":
     ax = model_out[["Legitimacy"]].plot()
@@ -67,11 +47,7 @@ if legitimacy_kind != "Local":
     ax.set_ylabel('Number of Citizens')
     _ = ax.legend(bbox_to_anchor=(1.35, 1.025))
     plt.tight_layout()
-<<<<<<< HEAD
     plt.savefig(r"C:\Users\Gebruiker\OneDrive\Computational_Science\Year1_Semester1_Block3\ABM\figures_normalgrid/legit_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
-=======
-    plt.savefig("figures_normalgrid/legit_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
     plt.show()
 
 agent_out = model.datacollector.get_agent_vars_dataframe()
@@ -88,11 +64,7 @@ if legitimacy_kind == "Local":
     ax.set_xlabel('Step')
     ax.set_ylabel('Number of Citizens')
     _ = ax.legend(bbox_to_anchor=(1.35, 1.025))
-<<<<<<< HEAD
     plt.savefig(r"C:\Users\Gebruiker\OneDrive\Computational_Science\Year1_Semester1_Block3\ABM\figures_normalgrid/legit_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
-=======
-    plt.savefig("figures_normalgrid/legit_"+legitimacy_kind+"_"+str(cop_density)+"_"+str(smart_cops)+".png")
->>>>>>> ba046af7f527081048e3aba427dc1b37f9995a43
     plt.tight_layout()
     plt.show()
 
